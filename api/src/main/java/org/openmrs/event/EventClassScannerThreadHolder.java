2d355b3f9b5c884eb0f01a7ee4c5cd93235c1586
Ian <ian.c.bacher@gmail.com>
2025-11-25 13:26:42 -0500

    Add docs for EventClassScannerThreadHolder

--- api/src/main/java/org/openmrs/event/EventClassScannerThreadHolder.java
+++ api/src/main/java/org/openmrs/event/EventClassScannerThreadHolder.java
@@ -2,6 +2,17 @@ package org.openmrs.event;
 
 import java.util.Optional;
 
+/**
+ * This class holds an instance of a {@link EventClassScanner} meant to be shared across a single thread. The instance
+ * is instantiated when constructing this class and is removed when calling the {@link #close()} method, so that it
+ * can be used in the try-with-resources pattern.
+ * <br/><br/>
+ * The intended use is: <pre>{@code
+ *  try (EventClassScannerThreadHolder holder = new EventClassScannerThreadHolder()) {
+ *      // do something that needs an EventClassScanner like subscribing or unsubscribing
+ *  }
+ *  }</pre>
+ */
 public class EventClassScannerThreadHolder implements AutoCloseable {
 
     private static final ThreadLocal<EventClassScanner> eventClassScannerHolder = new ThreadLocal<>();
